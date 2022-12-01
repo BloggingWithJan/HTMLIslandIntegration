49ca37c0b342e8c50db820bf9db1e366a7f61537
BloggingWithJan <118006052+BloggingWithJan@users.noreply.github.com>
2022-12-01 17:15:04 +0100

    Update Component.js

--- webapp/Component.js
+++ webapp/Component.js
@@ -17,24 +17,30 @@ sap.ui.define([
 		 * @override
 		 */
 		init: function () {
-			// call the base component's init function
-			UIComponent.prototype.init.apply(this, arguments);
-
-			// enable routing
-			this.getRouter().initialize();
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
 
-			// store htmlisland propertys in an model
+			// store htmlisland propertys in a model
 			this.setModel(models.createHTMLIslandModel(), "htmlisland");
+
+			// only use when using the index_option2.html [START]
 			this.getModel("htmlisland").setProperty("/propertyA", app.getPropertyA());
 			this.getModel("htmlisland").setProperty("/propertyB", app.getPropertyB());
+			// only use when using the index_option2.html [END]
 
+			// call the base component's init function - after creating htmlIsland Model - index.html option
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// enable routing
+			this.getRouter().initialize();
 		},
-		// setIslandPropertys: function (sPropertyA, sPropertyB) {
-		// 	this.oCompModel.setProperty("/propertyA", sPropertyA);
-		// 	this.oCompModel.setProperty("/propertyB", sPropertyB);
-		// }
+		/**
+		 * @description function gets called through index.html
+		 */
+		setIslandPropertys: function (sPropertyA, sPropertyB) {
+			this.getModel("htmlisland").setProperty("/propertyA", sPropertyA);
+			this.getModel("htmlisland").setProperty("/propertyB", sPropertyB);
+		}
 	});
 });
