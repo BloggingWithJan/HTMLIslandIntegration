711a25aa239dc15b40499c31244a9bfefe50d9df
BloggingWithJan <118006052+BloggingWithJan@users.noreply.github.com>
2022-12-01 17:14:19 +0100

    Update View1.controller.js

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,18 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sample/HTMLIsland/model/models"
+], function (Controller, models) {
 	"use strict";
 
 	return Controller.extend("sample.HTMLIsland.controller.View1", {
 		onInit: function () {
-
+			// store htmlisland propertys in a model
+			this.getView().setModel(models.createHTMLIslandModel(), "htmlislandoption3"); //index_option3 variant
 		},
 		fireEvent: function () {
 			app.fireServerEvent("event");
 		},
-		// setIslandPropertys: function (sPropertyA, sPropertyB) {
-		// 	this.getOwnerComponent().getModel("htmlisland").setProperty("/propertyA", sPropertyA);
-		// 	this.getOwnerComponent().getModel("htmlisland").setProperty("/propertyB", sPropertyB);
-		// }
+		/**
+		 * @description function gets called through index_option3.html
+		 */
+		setIslandPropertys: function (sPropertyA, sPropertyB) {
+			this.getView().getModel("htmlislandoption3").setProperty("/propertyA", sPropertyA);
+			this.getView().getModel("htmlislandoption3").setProperty("/propertyB", sPropertyB);
+		}
 	});
 });
